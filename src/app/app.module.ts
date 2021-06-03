c4f3eea36817951ee02f61960278d13bf7a8ac8d
Foxar <mostek3pl@gmail.com>
2021-06-03 09:45:57 +0200

    Added http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,6 +26,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     MatCardModule,
     MatToolbarModule,
     MatButtonModule,
