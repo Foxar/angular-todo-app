62532ce893d803ee14dc4da0eb019751edb55d7e
Foxar <mostek3pl@gmail.com>
2021-06-04 10:39:46 +0200

    Changed task from an interface to a class.

--- src/app/task.ts
+++ src/app/task.ts
@@ -1,8 +1,10 @@
-export interface Task {
-    id: number;
-    content: string;
-    dateAdded: [number,number,number];   //Day,Month,Year
-    checked: boolean;
+export class Task {
+    constructor(
+    public content: string,
+    public dateAdded: {day: number, month: number, year: number},
+    public checked: boolean,
+    public id?: number
+    ){}
 }
 
 
