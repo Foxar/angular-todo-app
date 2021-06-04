091852f5c8e2f95c0754235a9f63829fd5ba36e2
Foxar <mostek3pl@gmail.com>
2021-06-04 10:40:22 +0200

    Added formsmodule import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { DateHeaderComponent } from './date-header/date-header.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
 
     MatCardModule,
     MatToolbarModule,
