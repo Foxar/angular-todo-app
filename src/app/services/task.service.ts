da7107acd5785574fa102d71d16d54685f8df1ae
Foxar <mostek3pl@gmail.com>
2021-06-04 12:03:08 +0200

    Updated task service for new json-server dateAdded format, and added new task submission method.

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,15 +4,30 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Task} from '../task';
 import {environment} from '../../environments/environment';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
   private tasksEndpoint = environment.apiUrl.concat('/tasks');
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };  
 
   constructor(private httpClient:HttpClient) { }
 
   getTasks(selectedDate: Date): Observable<Task[]>{
-    return this.httpClient.get<Task[]>(this.tasksEndpoint.concat("?dateAdded=" + selectedDate.toDateString()));
+    return this.httpClient.get<Task[]>(
+      this.tasksEndpoint.concat(
+        "?dateAdded.day=" + selectedDate.getDate() + 
+        "&dateAdded.month=" + selectedDate.getMonth() +
+        "&dateAdded.year=" + selectedDate.getFullYear()));
+  }
+
+  submitTask(task: Task): Observable<Task>{
+    return this.httpClient.post<Task>(this.tasksEndpoint,task, this.httpOptions);
   }
 }
