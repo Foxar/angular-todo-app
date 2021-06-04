40056d5ecafba7d03736da9444e013e21613c0d2
Foxar <mostek3pl@gmail.com>
2021-06-04 11:02:05 +0200

    Correct reading of injected date value.

--- src/app/add-task-dialog/add-task-dialog.component.ts
+++ src/app/add-task-dialog/add-task-dialog.component.ts
@@ -1,22 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
 import {Task} from '../task';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-task-dialog',
   templateUrl: './add-task-dialog.component.html',
   styleUrls: ['./add-task-dialog.component.scss']
 })
-export class AddTaskDialogComponent {
+export class AddTaskDialogComponent implements OnInit {
 
-    constructor() { 
-
-    }
-    @Input() date: Date = new Date();
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {date: Date}) { }
+    date: Date = this.data.date;
 
     submitted = false;
 
-    model = new Task("",{day:0,month:0,year:0},false);
+    model = new Task(
+      "",
+      {day:0,month:0,year:0},
+      false
+    );
+
+    ngOnInit(){
+      this.model.dateAdded = {
+        day: this.date.getDate(),
+        month: this.date.getMonth(),
+        year: this.date.getFullYear()
+      }
+    }
     onSubmit(){
       this.submitted = true;
     }
